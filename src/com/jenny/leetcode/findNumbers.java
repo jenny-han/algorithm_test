3af766a478734175dbb30d7a3db9e1fb4039756e
jenny-han <dev.jennyhan@gmail.com>
2022-01-01 15:51:13 +0000

    findNumbers commit

--- src/com/jenny/leetcode/findNumbers.java
+++ src/com/jenny/leetcode/findNumbers.java
@@ -1,4 +1,14 @@
 package com.jenny.leetcode;
 
 public class findNumbers {
+    public int findNumbers(int[] nums) {
+        int count = 0;
+        for (int i =0; i<nums.length ; i ++) {
+            if((((int)(Math.log10(nums[i]))+1))%2 == 0){
+                count++;
+            }
+            
+        }
+        return count;
+    }
 }
